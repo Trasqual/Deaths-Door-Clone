b500a48e77d7ef4545ac83bead0c5a200de2804d
Trasqual <59961702+Trasqual@users.noreply.github.com>
2022-07-24 18:09:48 +0300

    Edit: Working on Failling Floor Puzzle Piece

--- Assets/_Main/Scripts/GamePlay/PuzzlePieces/FallingFloor.cs
+++ Assets/_Main/Scripts/GamePlay/PuzzlePieces/FallingFloor.cs
@@ -1,17 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 namespace _Main.Scripts.GamePlay.Puzzles
 {
+    [SelectionBase]
     public class FallingFloor : MonoBehaviour
     {
-        private void OnCollisionEnter(Collision collision)
+        [SerializeField] private float timeBeforeFall = 0.5f;
+
+        [SerializeField] private Transform _visual;
+        [SerializeField] private BoxCollider _col;
+
+        private Vector3 _visualStartPos;
+        private bool _isActive;
+
+        private void Start()
         {
-            if (collision.transform.TryGetComponent(out CharacterController player))
+            _visualStartPos = _visual.localPosition;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (_isActive) return;
+            if (other.TryGetComponent(out CharacterController player))
             {
-                Debug.Log("player detected");
+                ActivateFloor();
             }
+        }
+
+        private void ActivateFloor()
+        {
+            _isActive = true;
+
+            Sequence s = DOTween.Sequence();
+            s.Append(_visual.DOShakePosition(timeBeforeFall, 0.05f, 10, 60).OnComplete(() => _col.enabled = false));
+            s.Append(_visual.DOMoveY(-30f, 3f).SetRelative().OnComplete(() => _visual.gameObject.SetActive(false)));
+            s.AppendInterval(3f);
+            s.OnComplete(() =>
+            {
+                ResetFloor();
+            });
+        }
+
+        private void ResetFloor()
+        {
+            _visual.localPosition = _visualStartPos;
+            _visual.gameObject.SetActive(true);
+            _col.enabled = true;
+            _isActive = false;
         }
     }
 }
